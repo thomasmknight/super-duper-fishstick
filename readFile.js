7630250f915533384df90a73d6e218c61caf9797
thomasmknight <119625083+thomasmknight@users.noreply.github.com>
2023-02-11 13:23:33 -0600

    first-commit

---
+++ readFile.js
@@ -0,0 +1,11 @@
+const fs = require('node:fs/promises');
+
+async function readFile(filePath) {
+    try {
+        const data = await fs.readFile(filePath);
+        console.log(data.toString());
+    } catch (error) {
+        console.error(`Got an error trying to read the file: ${error.message}`);
+    }
+}
+readFile('greetings.txt');
